d0be2a21adbd2b115e3eb3a1d6507b9ab63f4a7b
Sam Foulger <samfoulger@gmail.com>
2020-07-01 17:42:52 -0700

    Initial Commit

---
+++ 2020-07-01 Mob/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2020-07-01 Mob
+//
+//  Created by selfoulger on 7/1/20.
+//  Copyright © 2020 Sam Foulger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 2020-07-01 MobTests/_020_07_01_MobTests.swift
@@ -0,0 +1,34 @@
+//
+//  _020_07_01_MobTests.swift
+//  2020-07-01 MobTests
+//
+//  Created by selfoulger on 7/1/20.
+//  Copyright © 2020 Sam Foulger. All rights reserved.
+//
+
+import XCTest
+@testable import _020_07_01_Mob
+
+class _020_07_01_MobTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
